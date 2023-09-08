298af67308e70e6541415a8e12e44fd35dc34645
KevinED11 <mendez_kael@hotmail.com>
2023-09-08 13:52:08 -0700

    types

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,29 @@ fn main() {
     // Variables
     let name = "kevin";
     let age:i8 = 22;
+
+
+    // Types
+    let company_string = "TutorialsPoint";  // string type
+    let rating_float = 4.5;                 // float type
+    let rating_float2: f32 = 4.5;                // float type
+    let is_growing_boolean = true;          // boolean type
+    let is_fun: bool = true;                      // boolean type
+    let icon_char = '♥';                    // unicode character type
+    let emoji:char = '😁';                         //  unicode character type
+
+    println!("company name is:{}",company_string);
+    println!("company rating on 5 is:{}",rating_float);
+    println!("company is growing :{}",is_growing_boolean);
+    println!("company icon is:{}",icon_char);
+
+    let result = 10;    // i32 by default
+    let age:u32 = 20; // unsigned
+    let sum:i32 = 5-15;
+    let mark:isize = 10;
+    let count:usize = 30;
+
+    println!("result value is {}",result);
+    println!("sum is {} and age is {}",sum,age);
+    println!("mark is {} and count is {}",mark,count);
 }
